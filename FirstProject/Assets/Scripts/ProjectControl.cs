36e1c840dd401a1f9d7c86a719b3e6d653fcec76
15905925236 <1687240871@qq.com>
2020-10-07 15:24:59 +0800

    UI界面切换功能

---
+++ FirstProject/Assets/Scripts/ProjectControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+
+public class ProjectControl:MonoBehaviour
+{
+    public GameObject Beforepanel;
+    public GameObject AfterPanel;
+    private void Start()
+    {
+        CloseInterface();
+        ShowInterface();
+    }
+    public void CloseInterface()
+    {
+        //关闭第一个界面
+        Beforepanel.SetActive(false);
+    }
+
+    public void ShowInterface()
+    {
+        //打开第二个界面
+        AfterPanel.SetActive(true);
+    }
+}
