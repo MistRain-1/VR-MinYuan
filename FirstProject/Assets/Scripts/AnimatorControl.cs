fcfb5e7ca86da4f399a529d8635e363fc4951a7e
DESKTOP-TCP6R2K\ds <1402013742@qq.com>
2020-10-13 15:08:17 +0800

    AudioControl,LoadManager,AnimatorControl代码，属性的设置。

---
+++ FirstProject/Assets/Scripts/AnimatorControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animation anim;
+    public string AnimName;  //动画的名字
+    void Start()
+    {
+        anim = this.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void AnimStart()
+    {
+        anim.Play(AnimName);
+    }
+    public void AnimStop()
+    {
+        anim.Stop(AnimName);
+    }
+
+}
---
+++ FirstProject/Assets/Scripts/AudioControl.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AudioSource audi;
+    void Start()
+    {
+        audi = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void AudioStart()
+    {
+        audi.Play();
+    }
+    public void AudioStop()
+    {
+        audi.Stop();
+    }
+}
